45db4d446889805a9a8c989f44e2a3ab9b2e546d
Piotr Balwierz <balwierz@gmail.com>
2022-08-12 22:27:54 +0200

    Create 2317 maximum XOR after operations.py

---
+++ 2317 maximum XOR after operations.py
@@ -0,0 +1,7 @@
+class Solution:
+    def maximumXOR(self, nums: List[int]) -> int:
+        #ret = 0
+        #for n in nums:
+        #    ret |= n
+        #return ret
+        return reduce(operator.or_, nums)
