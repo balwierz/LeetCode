65d323b7e887b621457c30aba4ae348b37795d44
Piotr Balwierz <balwierz@gmail.com>
2022-08-12 15:31:24 +0200

    Create 235 LCA of a BST.py

---
+++ 235 LCA of a BST.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        (p, q) = sorted([p.val, q.val])
+        while(root):
+            if p<=root.val and q>=root.val:
+                return root
+            if(q<root.val):
+                root = root.left
+            else:
+                root = root.right
+        
