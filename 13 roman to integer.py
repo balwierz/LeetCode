42ac966dad65d5bd504666eaaf647d7275847be2
Piotr Balwierz <balwierz@gmail.com>
2022-08-15 11:49:56 +0200

    Create 13 roman to integer.py

---
+++ 13 roman to integer.py
@@ -0,0 +1,45 @@
+class Solution:
+    def romanToInt2(self, s: str) -> int:
+        map = (
+            ("M", 1000),
+            ("CM", 900),
+            ("D", 500),
+            ("CD", 400),
+            ("C", 100),
+            ("XC", 90),
+            ("L", 50),
+            ("XL", 40),
+            ("X", 10),
+            ("IX", 9),
+            ("V", 5),
+            ("IV", 4),
+            ("I", 1)
+        )
+        i = 0
+        ret = 0
+        while i<len(s):
+            for roman, num in map:
+                if s[i:i+len(roman)] == roman:
+                    ret += num
+                    i += len(roman)
+                    break
+                    
+        return ret
+    
+    def romanToInt(self, s: str) -> int:
+        translations = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+        number = 0
+        s = s.replace("IV", "IIII").replace("IX", "VIIII")
+        s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
+        s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
+        for char in s:
+            number += translations[char]
+        return number
