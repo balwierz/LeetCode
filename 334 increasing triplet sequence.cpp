93206d98a7104b9ffbe6fa5e40b1a5c37366f013
Piotr Balwierz <balwierz@gmail.com>
2022-10-11 14:48:22 +0200

    Create 334 increasing triplet sequence.cpp

---
+++ 334 increasing triplet sequence.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums)
+    {
+        int minVal = INT_MAX, twoVal = INT_MAX;
+        for(int num : nums)
+        {
+            if(num > twoVal) return true;
+            if(num < minVal) minVal = num;
+            else if(minVal < num) twoVal = num;
+        }
+        return false;
+    }
+};
