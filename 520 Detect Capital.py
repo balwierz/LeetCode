cf0fe4f07766431d3c856a247c317b38110537b2
Piotr Balwierz <balwierz@gmail.com>
2023-01-02 11:23:54 +0100

    Create 520 Detect Capital.py

---
+++ 520 Detect Capital.py
@@ -0,0 +1,8 @@
+class Solution:
+    def detectCapitalUse(self, word: str) -> bool:
+        if len(word) == 1:
+            return True
+        if word[0].islower():
+            return word[1:].islower()
+        else:
+            return word[1:].islower() or word[1:].isupper()
