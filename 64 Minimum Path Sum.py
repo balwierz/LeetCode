f045a59664c0595fca7675892e7b3a0b7977147b
Piotr Balwierz <balwierz@gmail.com>
2023-03-27 16:01:45 +0200

    Create 64 Minimum Path Sum.py

---
+++ 64 Minimum Path Sum.py
@@ -0,0 +1,10 @@
+def minPathSum(self, grid: List[List[int]]) -> int:
+    m, n = len(grid), len(grid[0])
+    for j in range(1, n):
+        grid[0][j] += grid[0][j-1]
+    for i in range(1, m):
+        grid[i][0] += grid[i-1][0]
+        for j in range(1, n):
+            x = min(grid[i-1][j], grid[i][j-1])
+            grid[i][j] += x
+    return grid[m-1][n-1]
