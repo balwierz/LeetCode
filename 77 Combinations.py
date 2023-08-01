ba5dba2720a19c46a473b760e4d70e9600076701
Piotr Balwierz <balwierz@gmail.com>
2023-08-01 15:17:27 +0200

    Create 77 Combinations.py

---
+++ 77 Combinations.py
@@ -0,0 +1,7 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        if k == 1:
+            return [[i] for i in range(1, n+1)]
+        if k == n:
+            return [[i for i in range(1, n+1)]]
+        return [elem + [j] for j in range(k, n+1) for elem in self.combine(j-1, k-1)]
