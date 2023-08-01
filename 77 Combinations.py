d885d1c0a4fad514997fe3bc6b5bbb902d60c08c
Piotr Balwierz <balwierz@gmail.com>
2023-08-01 15:26:58 +0200

    Update 77 Combinations.py

--- 77 Combinations.py
+++ 77 Combinations.py
@@ -5,3 +5,9 @@ class Solution:
         if k == n:
             return [[i for i in range(1, n+1)]]
         return [elem + [j] for j in range(k, n+1) for elem in self.combine(j-1, k-1)]
+
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        if k == 0:
+            return [[]]
+        return [elem + [j] for j in range(k, n+1) for elem in self.combine(j-1, k-1)]
