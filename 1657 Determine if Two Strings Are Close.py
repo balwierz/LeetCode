299b38ac43eeb0420fa5aece45920d0b6f2223c5
Piotr Balwierz <balwierz@gmail.com>
2024-01-14 17:20:55 +0100

    Update 1657 Determine if Two Strings Are Close.py

--- 1657 Determine if Two Strings Are Close.py
+++ 1657 Determine if Two Strings Are Close.py
@@ -6,3 +6,9 @@ class Solution:
         if set(c1.keys()) != set(c2.keys()): return False
         if( sorted(c1.values()) != sorted(c2.values())): return False
         return True
+    # this is much faster
+    def closeStrings(self, w1: str, w2: str) -> bool:
+        if len(w1) != len(w2): return False
+        a = set(w1) 
+        if a != set(w2): return False
+        return sorted(w1.count(x) for x in a) == sorted(w2.count(x) for x in a)
