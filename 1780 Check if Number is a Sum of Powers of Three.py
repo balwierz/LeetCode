9bd48d60e035ba6166227df9d690237bd5987f6a
Piotr Balwierz <balwierz@gmail.com>
2025-03-04 23:17:39 +0000

    Update 1780 Check if Number is a Sum of Powers of Three.py

--- 1780 Check if Number is a Sum of Powers of Three.py
+++ 1780 Check if Number is a Sum of Powers of Three.py
@@ -5,3 +5,7 @@ class Solution:
             if r == 2:
                 return False
         return True
+
+class Solution:
+    def checkPowersOfThree(self, n: int) -> bool:
+        return n == 0 or (n % 3 < 2 and self.checkPowersOfThree(n // 3))
